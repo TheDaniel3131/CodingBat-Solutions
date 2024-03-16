bc78046d603965430d469c8d5ee43f0572925298
zephrous <danielpoh3131@gmail.com>
2024-03-16 18:29:09 +0800

    update

---
+++ Java/Warmup/7/solve.java
@@ -0,0 +1,21 @@
+// two methods
+
+public boolean startHi(String str) {
+    return str.startsWith("hi");
+}
+
+
+// or
+
+//public boolean startHi(String str) {
+//  if (str.length() < 2){
+//    return false;
+//  }
+//  String hi = str.substring(0,2);
+//
+//  if (hi.equals("hi")) {
+//    return true;
+//  } else {
+//    return false;
+//  }
+//}
