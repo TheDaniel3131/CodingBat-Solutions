cf6d73139104fa1e2aa39d98b9a75bd31da140c2
zephrous <danielpoh3131@gmail.com>
2024-04-10 19:14:22 +0800

    update

---
+++ Java/Warmup/12/solve.java
@@ -0,0 +1,10 @@
+public int max1020(int a, int b) {
+  if ((a >= 10 && a <= 20) && (b >= 10 && b <= 20)) {
+    return Math.max(a, b);
+  } else if (a >= 10 && a <= 20) {
+    return a;
+  } else if (b >= 10 && b <= 20) {
+    return b;
+  }
+  return 0;
+}
