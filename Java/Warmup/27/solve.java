9e3db06ca7eb3c42a41f2df6afdaa93726e459ed
zephrous <71692327+TheDaniel3131@users.noreply.github.com>
2024-05-06 15:05:41 +0800

    update

--- Java/Warmup/27/solve.java
+++ Java/Warmup/27/solve.java
@@ -4,3 +4,16 @@ public String front3(String str) {
   }
   return str.substring(0, 3) + str.substring(0, 3) + str.substring(0, 3);
 }
+
+public String front3(String str) {
+  String front;
+  
+  if (str.length() >= 3) {
+    front = str.substring(0, 3);
+  }
+  else {
+    front = str;
+  }
+
+  return front + front + front;
+}
